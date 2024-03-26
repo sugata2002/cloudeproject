deeefabf3fed5f8ed204ce46c58d09a7f2c25390
ronit <ronit@xlayer.in>
2024-03-27 00:56:39 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://localhost:4200'
+  apiUrl: 'http://localhost:4200',
+  baseUrl: 'https://cloudshop-zeta.vercel.app'
 };
